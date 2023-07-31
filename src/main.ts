e51f6746b2047423f8414419c7b78ca380422df7
Edilo Silva <edilo.silva@indt.org.br>
2023-07-31 13:25:08 -0400

    configuração de PORT

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
 
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+
+  await app.listen(port);
 }
 bootstrap();
